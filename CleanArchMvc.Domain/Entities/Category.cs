17c42ec23eb854f67012199e0c3de534faad687e
joaovitorcavalcante <joaovitor.c.barbosa9888@gmail.com>
2021-10-28 09:26:30 -0300

    initial commit

---
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -0,0 +1,42 @@
+﻿using CleanArchMvc.Domain.Validations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Entities
+{
+    public sealed class Category : Entity
+    {
+        public string Name { get; private set; }
+        public ICollection<Product> Products { get; private set; }
+
+        public Category(string name)
+        {
+            ValidateDomain(name);
+        }
+
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid Id value");
+            ValidateDomain(name);
+        }
+
+        public void Update(string name)
+        {
+            ValidateDomain(name);
+        }
+
+        private void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), 
+                "Invalid Name");
+
+            DomainExceptionValidation.When(name.Length < 3,
+                "Invalid Name, too short, minimum 3 characters");
+
+            Name = name;
+        }
+    }
+}
